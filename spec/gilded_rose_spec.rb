0bec7a1fb1e5beb530eb586e8d8d080c3772fef1
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-30 16:16:33 +0000

    Passing tests for normal items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,21 +10,36 @@ describe GildedRose do
       expect(items[0].name).to eq "foo"
     end
 
-    it "reduces the sell in value by 1 each day" do
-      item = Item.new("item", 5, 10)
-      items = [item]
-      gilded_rose = GildedRose.new(items)
-      gilded_rose.update_quality
-      expect(item.sell_in).to eq(4)
+    describe "normal items" do
+
+      it "reduces the sell in value by 1 each day" do
+        items = [Item.new("item", 5, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(4)
+      end
+
+      it "reduces the item quality by 1 each day" do
+        items = [Item.new("item", 5, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(9)
+      end
+
+      it "maxes out the item quality at 0" do
+        items = [Item.new("item", 1, 1)]
+        gilded_rose = GildedRose.new(items)
+        gilded_rose.update_quality
+        gilded_rose.update_quality
+        expect(items[0].quality).to eq(0)
+      end
+
+      it "reduction in item quality doubles after sell by date has passed" do
+        items = [Item.new("item", 0, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(8)
+      end
     end
 
-    it "reduces the item quality by 1 each day" do
-      item = Item.new("item", 5, 10)
-      items = [item]
-      gilded_rose = GildedRose.new(items)
-      gilded_rose.update_quality
-      expect(item.quality).to eq(9)
-    end
+    
 
   end
 
