ff1e64e95d510ce8b44c6aa6bf20dd7305f24a15
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-30 16:18:21 +0000

    Passes tests for legendary items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -39,7 +39,21 @@ describe GildedRose do
       end
     end
 
-    
+    describe "legendary items" do
+
+      it "Legendary items do not reduce in quaility" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 50, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(50)
+      end
+
+      it "Legendary items sell by date does not change" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 50, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(50)
+      end
+
+    end
 
   end
 
