bfb286bffc587f3ffc370b7a3949296d3d98f0de
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-01-30 16:28:22 +0000

    passing tests for aged brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -52,8 +52,30 @@ describe GildedRose do
         GildedRose.new(items).update_quality
         expect(items[0].sell_in).to eq(50)
       end
-
     end
+
+    describe "Aged brie" do
+
+      it "increases in quality over time" do
+        items = [Item.new("Aged Brie", 10, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(11)
+      end
+
+      it "its quality increases by 2 after passing the sell by date" do
+        items = [Item.new("Aged Brie", 0, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(12)
+      end
+
+      it "should have a max quality of 50" do
+        items = [Item.new("Aged Brie", 6, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 50
+        expect(items[0].sell_in).to eq 5
+      end
+    end
+
 
   end
 
